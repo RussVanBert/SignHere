3c0e7b69ddb630e4ce358b578e54d2ff31b4178e
Russell Van Bert <russ_vb@yahoo.com.au>
2016-10-24 21:56:53 +1100

    Add touch id functionality.

--- SignHere/ViewController.swift
+++ SignHere/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
   @IBOutlet var webView: UIWebView!
+  @IBOutlet weak var authoriseButton: UIButton!
+  @IBOutlet weak var thumbImage: UIImageView!
 
   override func viewDidAppear(_ animated: Bool) {
     let targetURL = Bundle.main.url(forResource: "sample", withExtension: "pdf")!
@@ -21,6 +24,49 @@ class ViewController: UIViewController {
       webView.load(data!, mimeType: "application/pdf", textEncodingName: "utf-8", baseURL: baseUrl)
     } catch _ {
       print("Whoa.. a bug.")
+    }
+  }
+
+  @IBAction func authorise(_ sender: UIButton) {
+    let context = LAContext()
+    var error: NSError?
+    guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+      showAlertViewIfNoBiometricSensorHasBeenDetected()
+      return
+    }
+    
+    evaluate(context, sender)
+  }
+  
+  func evaluate(_ context: LAContext, _ sender: UIButton) {
+    context.evaluatePolicy(
+      .deviceOwnerAuthenticationWithBiometrics,
+      localizedReason: "Sign this document?",
+      reply: { [unowned self] (success, error) -> Void in
+        if (success) {
+          self.showAlertWithTitle("Awesome", message: "Thanks for signing this document.")
+          
+        } else {
+          if let error = error {
+            self.showAlertWithTitle("Error", message: error.localizedDescription)
+          }
+        }
+      }
+    )
+  }
+  
+  func showAlertViewIfNoBiometricSensorHasBeenDetected(){
+    showAlertWithTitle("Error", message: "This device does not have a TouchID sensor.")
+  }
+  
+  func showAlertWithTitle(_ title:String, message:String ) {
+    let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+    alertVC.addAction(okAction)
+    
+    DispatchQueue.main.async {
+      self.present(alertVC, animated: true, completion: nil)
     }
   }
 }
